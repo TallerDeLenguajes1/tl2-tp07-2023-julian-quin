c04925d4467825a7ff8e264364ed68180b0ba4b1
julian <jnsoretniuq1998@gmail.com>
2023-10-06 00:06:57 -0300

    primer commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace tl2_tp07_2023_julian_quin;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
